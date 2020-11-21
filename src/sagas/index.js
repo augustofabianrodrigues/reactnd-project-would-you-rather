f42df06c35621f78f4dc9d6989f46504391167be
Augusto Fabian Rodrigues <augustofabianrodrigues@gmail.com>
2020-11-21 19:22:21 -0300

    feat: Add sagas keepAlive

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,7 +1,43 @@
-import { all } from 'redux-saga/effects';
+import { all, call, spawn } from 'redux-saga/effects';
 import { watchAndLog } from './logger';
 import { handleInitialData } from './shared';
 
+/**
+ * Spawns given sagas, restarting them if they throw any uncaught error.
+ * @param  {...any} sagas The sagas to spawn and keep alive
+ */
+function* keepAlive(...sagas) {
+  yield all(
+    sagas.map((saga) =>
+      spawn(function* () {
+        while (true) {
+          try {
+            // Start the worker saga
+            yield call(saga);
+            // If it finishes, exit
+            break;
+          } catch (e) {
+            // If an error happens it will be logged
+            // and the loop will restart the saga
+            console.groupCollapsed(
+              `%cSaga ${saga.name} crashed and will be restarted...`,
+              `
+                font-size: 600;
+                color: #DC2626;
+                background-color: #FECACA;
+                padding: 0.125rem 0.25rem;
+                border-radius: 0.125rem;
+              `
+            );
+            console.error(e);
+            console.groupEnd();
+          }
+        }
+      })
+    )
+  );
+}
+
 export default function* rootSaga() {
-  yield all([watchAndLog(), handleInitialData()]);
+  yield keepAlive(watchAndLog, handleInitialData);
 }
