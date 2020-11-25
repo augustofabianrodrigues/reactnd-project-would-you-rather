0653f433218d5f814942e0319d6b8a871c17d142
Augusto Fabian Rodrigues <augustofabianrodrigues@gmail.com>
2020-11-24 23:03:23 -0300

    feat: Add auth pages transitions

--- src/components/auth/Auth.js
+++ src/components/auth/Auth.js
@@ -5,12 +5,13 @@ import SignIn from './SignIn';
 import SignUp from './SignUp';
 import { pathJoin } from '../../utils/helpers';
 import AuthFooter from './AuthFooter';
+import { CSSTransition, SwitchTransition } from 'react-transition-group';
 
 function Auth(props) {
-  const { match } = props;
+  const { location, match } = props;
 
   return (
-    <div className="overflow-y-auto h-full w-full flex flex-col lg:flex-row">
+    <div className="overflow-x-hidden overflow-y-auto h-full w-full flex flex-col lg:flex-row">
       <header className="flex flex-col flex-shrink lg:flex-shrink-0 lg:flex-grow lg:justify-center xl:items-center p-4 lg:p-8 lg:space-y-20 bg-gray-100 dark:bg-gray-900 shadow-inner">
         <div className="lg:space-y-4">
           <h2 className="lg:text-xl xl:text-4xl">Welcome to</h2>
@@ -25,18 +26,31 @@ function Auth(props) {
         />
       </header>
 
-      <div className="flex flex-col flex-grow space-y-12 lg:flex-grow-0 lg:flex-shrink min-w-max p-4 lg:p-8 bg-gray-50 dark:bg-gray-800">
+      <div className="flex flex-col flex-grow space-y-12 lg:flex-grow-0 lg:flex-shrink lg:min-w-max p-4 lg:p-8 bg-gray-50 dark:bg-gray-800">
         <main className="lg:w-96 mb-auto lg:m-auto">
-          <Switch>
-            <Route
-              exact
-              path={pathJoin(match.path, 'sign-up')}
-              component={SignUp}
-            />
-            <Route path={pathJoin(match.path, '*')}>
-              <SignIn signUpUrl={pathJoin(match.url, 'sign-up')} />
-            </Route>
-          </Switch>
+          <SwitchTransition mode="out-in">
+            <CSSTransition
+              key={location.pathname}
+              timeout={500}
+              classNames={{
+                enter: 'opacity-0',
+                enterActive: 'opacity-100 transition-opacity duration-500',
+                exitActive:
+                  'opacity-0 transform translate-x-full transition duration-500',
+              }}
+            >
+              <Switch location={location}>
+                <Route
+                  exact
+                  path={pathJoin(match.path, 'sign-up')}
+                  component={SignUp}
+                />
+                <Route path={pathJoin(match.path, '*')}>
+                  <SignIn signUpUrl={pathJoin(match.url, 'sign-up')} />
+                </Route>
+              </Switch>
+            </CSSTransition>
+          </SwitchTransition>
         </main>
 
         <AuthFooter />
@@ -46,6 +60,7 @@ function Auth(props) {
 }
 
 Auth.propTypes = {
+  location: PropTypes.object.isRequired,
   match: PropTypes.object.isRequired,
 };
 
