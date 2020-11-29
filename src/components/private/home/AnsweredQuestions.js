741865b8a64c64ea53a613e5943162db1763d563
Augusto Fabian Rodrigues <augustofabianrodrigues@gmail.com>
2020-11-29 17:08:26 -0300

    fix: Logout from home
    - Logging out from home would cause the app to crash.

--- src/components/private/home/AnsweredQuestions.js
+++ src/components/private/home/AnsweredQuestions.js
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import QuestionsGrid from './QuestionsGrid';
 
-function AnsweredQuestions({ answeredQuestions }) {
-  return <QuestionsGrid questions={answeredQuestions} />;
-}
+class AnsweredQuestions extends Component {
+  static propTypes = {
+    answeredQuestions: PropTypes.array.isRequired,
+  };
 
-AnsweredQuestions.propTypes = {
-  answeredQuestions: PropTypes.array.isRequired,
-};
+  shouldComponentUpdate(props) {
+    // If the user has logged out, this would be re-rendered without the user during the pages transition animation.
+    // Keep this component as is for the animation to finish with the user that was logged in.
+    return props.answeredQuestions !== null;
+  }
+
+  render() {
+    const { answeredQuestions } = this.props;
+    return <QuestionsGrid questions={answeredQuestions} />;
+  }
+}
 
 function mapStateToProps(state, props) {
   const authedUser = state.users[state.authedUser];
+  if (!authedUser) {
+    return {
+      ...props,
+      answeredQuestions: null,
+    };
+  }
+
   const answeredQuestions = Object.keys(state.questions)
     .filter((questionId) => questionId in authedUser.answers)
     .map((questionId) => {
--- src/components/private/home/UnansweredQuestions.js
+++ src/components/private/home/UnansweredQuestions.js
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import QuestionsGrid from './QuestionsGrid';
 
-function UnansweredQuestions({ unansweredQuestions }) {
-  return <QuestionsGrid questions={unansweredQuestions} />;
-}
+class UnansweredQuestions extends Component {
+  static propTypes = {
+    unansweredQuestions: PropTypes.array,
+  };
 
-UnansweredQuestions.propTypes = {
-  unansweredQuestions: PropTypes.array.isRequired,
-};
+  shouldComponentUpdate(props) {
+    // If the user has logged out, this would be re-rendered without the user during the pages transition animation.
+    // Keep this component as is for the animation to finish with the user that was logged in.
+    return props.unansweredQuestions !== null;
+  }
+
+  render() {
+    const { unansweredQuestions } = this.props;
+
+    return <QuestionsGrid questions={unansweredQuestions} />;
+  }
+}
 
 function mapStateToProps(state, props) {
   const authedUser = state.users[state.authedUser];
+  if (!authedUser) {
+    return {
+      ...props,
+      unansweredQuestions: null,
+    };
+  }
+
   const unansweredQuestions = Object.keys(state.questions)
     .filter((questionId) => !(questionId in authedUser.answers))
     .map((questionId) => {
